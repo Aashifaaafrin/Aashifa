cbd4886be80ac6f94f6a7d1eb87bed294241ca48
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-07-18 16:16:16 +0530

    Create LargestNum.java

---
+++ LargestNum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LargestNum {
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the numbers:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		if(a>b&&a>c)
+		{
+			System.out.println(+a);
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println(+b);
+		}
+		else
+		{
+			System.out.println(+c);
+		}
+		
+	}
+
+}
+
