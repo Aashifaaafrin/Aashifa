7abb3f58ccc72af46b24265d1e569ff61551277a
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-07-18 19:33:26 +0530

    Create PrimeNot.java

---
+++ PrimeNot.java
@@ -0,0 +1,20 @@
+int num;
+Scanner s=new Scanner(System.in);
+num=s.nextInt();                        
+boolean flag = false;
+for(int i = 2; i <= num/2; ++i)
+{
+    if(num % i == 0)
+    {
+        flag = true;
+        break;
+    }
+}
+
+if (!flag)
+    System.out.println("yes");
+else
+    System.out.println("no");
+}
+}
+
