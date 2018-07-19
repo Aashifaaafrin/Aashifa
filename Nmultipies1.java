fbac80d82df33c9929ee3285210baecb0745ddb3
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-07-19 11:57:44 +0530

    Create Nmultipies1.java

---
+++ Nmultipies1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Nmultiplies {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=5;i++)
+		{
+			int a=n*i;
+			System.out.println(a);
+
+		}
+	}
+
+}
+
