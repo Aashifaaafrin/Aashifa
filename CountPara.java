bc105c7d0483376316d0f377f5ce6b0e88cb10ce
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-07-27 15:50:09 +0530

    Create CountPara.java

---
+++ CountPara.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CountPara {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String n=s.nextLine();
+		String m[]=n.split("\\.");
+			int a=m.length;
+		System.out.println(a);
+
+	}
+
+}
