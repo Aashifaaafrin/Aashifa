c90e524fd14e86c7f578fc0257b3e26fa147d08d
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-07-31 16:15:23 +0530

    Create StringConcatination.java

---
+++ StringConcatination.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StringConcatination {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		String s2=s.next();  
+		String s3;
+		{
+			  s3=s1+s2;
+		}
+		System.out.println(s3);
+	}
+
+}
