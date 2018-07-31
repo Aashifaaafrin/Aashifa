6b9b309e208d6da09ed518b5b2cdc1434ccd37f3
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-07-31 14:58:11 +0530

    Create StringNum.java

---
+++ StringNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StringNum {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		String m=s.nextLine();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(m);
+
+		}
+	}
+
+}
