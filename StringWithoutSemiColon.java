9ed7e97ffaa9f1d6d2204aaead53a11305bc60c9
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-08-02 18:20:49 +0530

    Create StringWithoutSemiColon.java

---
+++ StringWithoutSemiColon.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StringWithoutSemiColon {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String n=s.next();
+        for(int i = 1; i < 2; System.out.println(n))
+        {
+        	i++;
+        }
+	}
+
+}
