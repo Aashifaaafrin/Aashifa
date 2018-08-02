c80d4be0940a36fa81ce2bb3f2f795748a620791
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-08-02 17:15:01 +0530

    Create FirstCharcters.java

---
+++ FirstCharcters.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FirstCharcters {
+     public static void main(String[] args) {
+    	 Scanner s=new Scanner(System.in);
+  		String m=s.nextLine();
+        int n=s.nextInt();
+ 		String s1=m.substring(0, n);
+ 			System.out.println(s1);
+
+ 	}
+
+ }
+
