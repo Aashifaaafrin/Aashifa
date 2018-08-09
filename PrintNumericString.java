3cf8428b8a1eec5671dee86562cad45d90dc5b53
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-08-09 14:39:42 +0530

    Create PrintNumericString.java

---
+++ PrintNumericString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PrintNumericString {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String n=s.next();
+		String m="";
+		for(int i=0;i<n.length();i++)
+		{
+			if(Character.isDigit(n.charAt(i)))
+			{
+				m+=n.charAt(i);
+			}
+		}
+		System.out.println(m);
+		
+	}
+
+}
