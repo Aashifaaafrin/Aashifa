a26b9d771389f4efc3351a969d9cad6a111e428f
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2018-08-10 13:58:55 +0530

    Create NumberReverse.java

---
+++ NumberReverse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumberReverse {
+	public static void main(String[] args) {
+		  int num=0;
+	       int reversenum =0;
+	       Scanner s=new Scanner(System.in);
+	       num = s.nextInt();
+	       while( num != 0 )
+	       {
+	          reversenum = reversenum * 10;
+	          reversenum = reversenum + num%10;
+	          num = num/10;
+	}
+
+	       System.out.println(reversenum);
+	}
+	}
+
