3aae85af4500e79b8d47e189c0b0c083a315cf37
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2019-01-29 03:19:58 -0800

    Create printEquation.java

---
+++ printEquation.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class printEquation {
+	public static void main(String args[])          
+	{
+		int a=2,b=2,c=3;
+       int d=(a*b)/c;
+       System.out.println(d);
+	}
+	
+}	
