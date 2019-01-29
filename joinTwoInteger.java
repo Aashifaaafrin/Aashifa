d2ef00734d1110a4e0b7c1606e015f100e83558e
Aashifaaafrin <40021979+Aashifaaafrin@users.noreply.github.com>
2019-01-29 02:52:35 -0800

    Create joinTwoInteger.java

---
+++ joinTwoInteger.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class joinTwoInteger {
+	public static void main(String[] args) {
+		int a=123;
+		int b=456;
+		int c = Integer.valueOf(String.valueOf(a) + String.valueOf(b));
+        System.out.println(c);
+	}
+
+}
